6e4d35e251d3b411b2060186297dec793619a1ab
candyman5667 <candyman5667@gmail.com>
2022-05-22 08:12:14 +0900

    firstcommit

--- AttendanceApp/database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ AttendanceApp/database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatUsersTable extends Migration
+class CreatePasswordResetsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreatUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function(Blueprint $table){
-            $table->id();
-            $table->string('name',191);
-            $table->string('email',191);
-            $table->string('password',191);
+        Schema::create('password_resets', function (Blueprint $table) {
+            $table->string('email')->index();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
         });
     }
 
@@ -28,6 +27,6 @@ class CreatUsersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('password_resets');
     }
 }
